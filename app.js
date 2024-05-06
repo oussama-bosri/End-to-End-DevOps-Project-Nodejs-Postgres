0a4e7c038fec4e49791763c7658f2ac58e4796fc
OUSSAMA <58121982+oussama-bosri@users.noreply.github.com>
2024-05-06 21:50:06 +0100

    Solving network problem

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const { Pool } = require('pg');
 const path = require('path');
 const app = express();
 
-// Connection parameters for your PostgreSQL database
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'host.docker.internal',
   database: process.env.DB_DATABASE || 'postgres_db',
   password: process.env.DB_PASSWORD || 'password',
   port: process.env.DB_PORT || 5432,
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the database');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 app.set('view engine', 'ejs');
@@ -28,6 +36,6 @@ app.get('/', async (req, res) => {
   }
 })
 
-app.listen(8000, () => {
-  console.log('Server is running at port 8000');
+app.listen(4000, () => {
+  console.log('Server is running at port 4000');
 });
